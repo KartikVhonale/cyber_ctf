277c4683673afa4c17ef902e1d967f03348d691d
arnavhax <113230128+arnavhax@users.noreply.github.com>
2023-09-27 18:27:34 +0000

    secrets added

--- database.py
+++ database.py
@@ -1,16 +1,17 @@
 from sqlalchemy import create_engine, text
-USER="qvonmg1ppdnsylw8ks0y"
-PASSWORD="pscale_pw_A55cImmJAXFpJrIXQjVF0Zf56TxzSoIf2iCoGocF7Tt"
-HOST="aws.connect.psdb.cloud"
-PORT=3306
-DATABASE="cyber_ctf"
+import os
+USER= os.environ['USER']
+PASSWORD= os.environ['PASSWORD']
+HOST=os.environ['HOST']
+PORT=os.environ['PORT']
+DATABASE=os.environ['DATABASE']
 # Replace the placeholders in the connection string with actual values
 connection_string = "mysql+pymysql://%s:%s@%s:%s/%s" % (USER, PASSWORD, HOST, PORT, DATABASE)
 
 engine = create_engine(connection_string, connect_args={"ssl": {"ssl_ca": "/etc/ssl/cert.pem"}})
 
-with engine.connect() as conn:
-    result = conn.execute(text("SELECT * FROM accounts"))
-    for row in result:
-        print(row)
+# with engine.connect() as conn:
+#     result = conn.execute(text("SELECT * FROM accounts"))
+#     for row in result:
+#         print(row)
 
