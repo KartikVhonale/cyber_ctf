31d2313b3404ba40fbac2d584601472e82fa593c
arnavhax <113230128+arnavhax@users.noreply.github.com>
2023-09-27 08:02:25 +0000

    global start fixed

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template
 
 app = Flask(__name__)
-global start=False
+start=False
 @app.route("/")#login is default route
 def login_render():
   return render_template('login.html')
